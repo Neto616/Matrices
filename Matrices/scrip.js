797ddf9695a5719148e87a9b71ae4c7f64cd3e3e
Neto616 <140271677+Neto616@users.noreply.github.com>
2023-07-28 11:46:11 -0600

    Add files via upload

--- Matrices/scrip.js
+++ Matrices/scrip.js
@@ -1,63 +1,40 @@
 const buttonClick = document.getElementById('btn');
 const matriz1 = document.querySelector('#A');
-const matriz2 = document.querySelector('B');
+const matriz2 = document.querySelector('#B');
 const respuesta = document.querySelector('#R');
+
 
 buttonClick.addEventListener('click', actionButton);
 
-    function actionButton(){
-        listenClickBtn(matriz1.value, matriz2.value)
+function actionButton(){
+        const tilin = listenClickBtn(matriz1.value)
+        const tilin2 = listenClickBtn(matriz2.value)
+        console.log(tilin)
+        console.log(tilin2)
     }
 
-    function listenClickBtn(matriz1, matriz2){
+    function listenClickBtn(matriz){
      let num= ""
+     let arregloAux = [];
      let arreglo = [];
-     
-     for (let i=0; i < matriz1.length; i++){
-        if(matriz1[i]==="\n"){
-            for(let j = 0; j < matriz1.length; j++)
-            if(matriz1[i]==="\n"){
-                arreglo [i][j] = num
-            }else if(i==matriz1.length-1){
-                num = num.concat(matriz1[i])
-                arreglo.push(num)
-            }else if(matriz1[i]!==","){
-                num = num.concat(matriz1[i])
-            }else if(matriz1[i] ==","){
-                arreglo.push(num)
-                num=""
-            }
-        }
-        else if(i==matriz1.length-1){
-            num = num.concat(matriz1[i])
-            arreglo.push(num)
-        }else if(matriz1[i]!==","){
-            num = num.concat(matriz1[i])
-        }else if(matriz1[i] ==","){
-            arreglo.push(num)
+
+
+     for (let i=0; i < matriz.length; i++){
+        if(matriz[i]=="\n"){
+            arregloAux.push(num)
+            arreglo.push(arregloAux);
+            arregloAux = []
+            num= ""
+        }else if(i==matriz.length-1){
+            num = num.concat(matriz[i])
+            arregloAux.push(num)
+            arreglo.push(arregloAux)
+        }else if(matriz[i]!==","){
+            num = num.concat(matriz[i])
+        }else if(matriz[i] ==","){
+            arregloAux.push(num)
             num=""
-        }
-        // else if(i==matriz1.length-1){
-        //     num = num.concat(matriz1[i])
-        //     arreglo.push(num)
-        // }else if(matriz1[i]!==","){
-        //     num = num.concat(matriz1[i])
-        // }else if(matriz1[i] ==","){
-        //     arreglo.push(num)
-        //     num=""
-        // }
-       
-     }
-     console.log(arreglo)
-        //  for(let i = 0; i < matriz1.length; i++){
-    //     if(matriz1[i]==='\n'){
-    //         console.log(palabra)
-    //         palabra = ""
-    //     }else if (i === matriz1.length-1){
-    //         palabra = palabra.concat(matriz1[i])
-    //         console.log(palabra)
-    //     }else{
-    //         palabra = palabra.concat(matriz1[i])
-    //     }
-    //  }
-    } 
+        } 
+    }
+    return arreglo
+} 
